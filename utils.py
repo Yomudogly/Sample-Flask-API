e29de23eff6cc3d68d4f559cedab1032bd841d6d
Yomudogly <khalnepesov@gmail.com>
2020-05-18 21:47:57 -0400

    readme fixed

--- utils.py
+++ utils.py
@@ -15,25 +15,3 @@ class APIException(Exception):
         rv = dict(self.payload or ())
         rv['message'] = self.message
         return rv
-    
-
-# def has_no_empty_params(rule):
-#     defaults = rule.defaults if rule.defaults is not None else ()
-#     arguments = rule.arguments if rule.arguments is not None else ()
-#     return len(defaults) >= len(arguments)
-
-# def generate_sitemap(app):
-#     links = []
-#     for rule in app.url_map.iter_rules():
-#         # Filter out rules we can't navigate to in a browser
-#         # and rules that require parameters
-#         if "GET" in rule.methods and has_no_empty_params(rule):
-#             url = url_for(rule.endpoint, **(rule.defaults or {}))
-#             links.append(url)
-
-#     links_html = "".join(["<li><a href='" + y + "'>" + y + "</a></li>" for y in links])
-#     return """
-#         <div style="text-align: center;">
-#         <img src='https://ucarecdn.com/3a0e7d8b-25f3-4e2f-add2-016064b04075/rigobaby.jpg' />
-#         <h1>Hello Rigo!!</h1>
-#         This is your api home, remember to specify a real endpoint path like: <ul style="text-align: left;">"""+links_html+"</ul></div>"
